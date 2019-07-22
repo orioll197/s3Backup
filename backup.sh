1cd16ed86a117c5b927418486b8ad7ac75224bc8
Oriol Hidalgo <oriol@offin.es>
2019-07-22 18:35:03 +0200

    Add backup of mysql table with mysqldump

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+hash mysqldump 2>/dev/null || { echo >&2 "You need to install mysqldump to continue. Aborting."; exit 1; }
+
 source ./credentials
 
 DATEAPP="date"
@@ -20,8 +22,13 @@ fi
 
 cd /tmp
 for i in $DIRS; do
-    FILENAME=${i##*/}
-    tar zcvf ${FILENAME}.tar.gz $i
-    aws s3 cp /tmp/${FILENAME}.tar.gz s3://${BUCKET}/${FILENAME}/${TODAY}/backup.tar.gz
+    DDBB_NAME=${i#*-->}
+    DIRECTORY=${i%-->*}
+    FILENAME=${DIRECTORY##*/}
+    tar zcvf ${FILENAME}.tar.gz $DIRECTORY
+    mysqldump -h $DDBB_ENDPOINT  -u $DDBB_USER -p$DDBB_PASSWORD --port=$DDBB_PORT     --single-transaction     --routines     --triggers     --databases  $DDBB_NAME > ${DDBB_NAME}.sql
+    aws s3 cp /tmp/${FILENAME}.tar.gz s3://${BUCKET}/${FILENAME}/${TODAY}/backup-${FILENAME}-${TODAY}.tar.gz
+    aws s3 cp /tmp/${DDBB_NAME}.sql s3://${BUCKET}/${FILENAME}/${TODAY}/backup-${DDBB_NAME}-${TODAY}.sql
     rm -rf /tmp/${FILENAME}.tar.gz
+    rm -rf /tmp/${DDBB_NAME}.sql
 done
